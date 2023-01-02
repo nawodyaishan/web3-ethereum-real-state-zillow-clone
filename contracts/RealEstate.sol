204e76372fead844a92096e0f61d2f1f5401f5ee
nawodyaishan <nawodyain@gmail.com>
2023-01-03 03:06:53 +0530

    Add Import ERC721

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,14 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RealEstate {
+// Importing ERC 721 Token Contract
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Escrow.sol";
 
+
+contract RealEstate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 }
