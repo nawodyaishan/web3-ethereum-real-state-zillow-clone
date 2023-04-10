aecc075132b8f89728fe0bb009503a6d070b11e8
nawodyaishan <nawodyain@gmail.com>
2023-04-10 19:06:04 +0530

    feat: add mint test

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,11 +6,21 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+
+    let buyer, seller
+    let realEstate
     it('should saves nft address', async function () {
+        // Setup accounts
+        [buyer, seller, inspector, lender] = await ethers.getSigners()
+
+        // Deploy realState
         const realEstateContract = await ethers.getContractFactory(`RealEstate`)
         realEstate = await realEstateContract.deploy()
         console.log(realEstate.address)
 
+        // Mint Fn
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+        await transaction.wait()
     });
 
 })
